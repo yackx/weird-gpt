0114130877a1d568710279cbd1fa41de2aede3c5
Youri Ackx <1193689+yackx@users.noreply.github.com>
2024-06-01 13:15:08 +0200

    Show random image on home screen

--- Weird_GPT.py
+++ Weird_GPT.py
@@ -1,3 +1,5 @@
+import random
+
 import streamlit as st
 
 from weird_gpt.load_keys import check_openai_api_key_set
@@ -14,4 +16,23 @@ except ValueError:
 
 st.title("💬 Weird GPT")
 st.subheader("Tweaked OpenAI ChatGPT assistants")
-st.markdown("Even tweaked, the chatbots may still hallucinate.")
+st.info("Even tweaked, the chatbots may still hallucinate.")
+
+images = [
+    "s_airplane_1",
+    "s_airplane_2",
+    "s_alien_2",
+    "s_alien_3",
+    "s_alien_4",
+    "s_beagle_beach_1",
+    "s_cat_destroys_city_1",
+    "s_cat_destroys_city_2",
+    "s_cat_destroys_city_3",
+    "s_dog_catching_pizza_2",
+    "s_dog_catching_pizza_3",
+    "s_dogs_fallout",
+    "s_dogs_pubg_1",
+    "s_munch"
+]
+random_image = random.choice(images)
+st.image(f"./assets/random/{random_image}.png", use_column_width=True)
